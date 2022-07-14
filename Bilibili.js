38d5dd40840ef6e7be8a123daea1057895bdad7c
quintin <554389585@qq.com>
2022-07-14 22:13:03 +0800

    zhihu.js bilibili.js 增加数据和logo缓存

--- Bilibili.js
+++ Bilibili.js
@@ -1,91 +1,71 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: pink; icon-glyph: heartbeat;
-/*
- * Author: evilbutcher
- * Github: https://github.com/evilbutcher
- * 本脚本使用了@Gideon_Senku的Env.scriptable，感谢！
- */
+// icon-color: light-gray; icon-glyph: award;
 
-const $ = importModule("Env");
+const common = importModule('common');
 var rid = 0; //rid对应不同的B站榜单：0全站，1动画，3音乐，4游戏，5娱乐，36科技，119鬼畜，129舞蹈。
-var num = 6; //自定义显示数量
-var rancolor = true; //true为开启随机颜色
+var num = 5; //自定义显示数量
+const jsonCacheSeconds = 300;
+const imgUseCache = true;
+const fm = FileManager.iCloud();
+const cachePath = fm.documentsDirectory() + "/app_icon";
 
 var biData = {
   "data": [{
     "title": "",
-    "uri": "",
+    "uri": ""
   }]
-}
-
-const res = await getinfo();
-
-let widget = createWidget(res);
-Script.setWidget(widget);
-Script.complete();
-
-function createWidget(res) {
-  var biData = res;
-  const w = new ListWidget();
-  const fm = FileManager.iCloud();
-  const path = fm.documentsDirectory() + '/mt.jpg';
-  w.backgroundImage = fm.readImage(path);
-  w.addSpacer();
-  w.spacing = 5;
-
-  const firstLine = w.addText(`哔哩哔哩排行榜`);
-  firstLine.font = new Font('SF Mono', 15);
-  firstLine.textColor = Color.white();
-  firstLine.textOpacity = 0.7;
-
-  for (var i = 0; i < num; i++) {
-    addTextToListWidget(i + 1, biData["data"][i], w);
-  }
-
-  w.addSpacer();
-  w.spacing = 5;
-  w.presentSmall();
-  return w;
-}
-
-async function getinfo() {
-  const blRequest = {
-    url: `https://app.bilibili.com/x/v2/rank/region?rid=${rid}`,
-  };
-  const res = await $.get(blRequest);
-  return res;
 }
 
 function addTextToListWidget(n, bilibiliData, listWidget) {
   let item = listWidget.addText(n + '. ' + bilibiliData.title);
   item.textColor = Color.white();
-  item.font = new Font('SF Mono', 12);
+  item.font = new Font('SF Mono', 15);
   item.url = bilibiliData.uri;
 }
 
-function color16() {
-  var r = Math.floor(Math.random() * 256);
-  if (r + 50 < 255) {
-    r = r + 50;
+const data = await common.getDataFromCache("BilibiliCache", `https://app.bilibili.com/x/v2/rank/region?rid=${rid}`, "GET", {}, {}, jsonCacheSeconds);
+const widget = await createWidget(data);
+if (!config.runsInWidget) {
+  await widget.presentMedium();
+}
+Script.setWidget(widget);
+Script.complete();
+
+async function createWidget(data) {
+  biData = data;
+  let w = new ListWidget();
+  // 创建标题
+  var stackHeader = w.addStack()
+  stackHeader.centerAlignContent()
+  // 添加 icon
+  var stackImgItem = stackHeader.addImage(await common.getImgFromCache("Bilibili_app_logo.png", "https://play-lh.googleusercontent.com/XSF7pMzahsz4HBYttQhS0LeWFfjcPxaIPdkSmiuud7qKEODAXFeUVct5J7DXdj-ovDc", imgUseCache, cachePath))
+  stackImgItem.imageSize = new Size(15, 15)
+  stackImgItem.cornerRadius = 5
+  stackHeader.addSpacer(3)
+  // 添加标题
+  var textItem = stackHeader.addText(`哔哩哔哩`)
+  textItem.font = new Font('SF Mono', 15);
+  textItem.textColor = Color.white()
+
+  // 添加更新时间
+  stackHeader.addSpacer()
+  var myDate = new Date();
+  var textItem = stackHeader.addText(`${myDate.getHours().toString().padStart(2, '0')}:${myDate.getMinutes().toString().padStart(2, '0')}更新`)
+  textItem.font = Font.regularMonospacedSystemFont(10)
+  textItem.textColor = Color.white()
+  textItem.rightAlignText()
+
+  for (var i = 0; i < num; i++) {
+    addTextToListWidget(i + 1, biData["data"][i], w);
   }
-  if (r > 230 && r < 255) {
-    r = r - 50;
-  }
-  var g = Math.floor(Math.random() * 256);
-  if (g + 50 < 255) {
-    g = g + 50;
-  }
-  if (g > 230 && g < 255) {
-    g = g - 50;
-  }
-  var b = Math.floor(Math.random() * 256);
-  if (b + 50 < 255) {
-    b = b + 50;
-  }
-  if (b > 230 && b < 255) {
-    b = b - 50;
-  }
-  var color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
-  return color;
+
+  w.spacing = 5;
+
+  // 设置透明壁纸 
+  const fm = FileManager.iCloud();
+  const path = fm.documentsDirectory() + '/mt.jpg';
+  await fm.downloadFileFromiCloud(path);
+  w.backgroundImage = fm.readImage(path);
+  return w;
 }
--- Zhihu.js
+++ Zhihu.js
@@ -1,100 +1,75 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-blue; icon-glyph: book-open;
-/*
- * Author: evilbutcher
- * Github: https://github.com/evilbutcher
- * 本脚本使用了@Gideon_Senku的Env.scriptable，感谢！
- */
-const goupdate = true;
-const $ = importModule("Env");
-var num = 6; //自定义显示数量
-var rancolor = true; //true为开启随机颜色
+// icon-color: light-gray; icon-glyph: award;
+
+const common = importModule('./common.js');
+
+// 图片是否使用缓存开关
+const imgUseCache = true;
+// 图片地址
+const fm = FileManager.iCloud();
+const cachePath = fm.documentsDirectory() + "/app_icon";
+const num = 5;
+const jsonCacheSeconds = 300;
 
 var zhihuData = {
   "data": [{
-    "target":{
-      "id":0,
-      "title":""
+    "target": {
+      "id": 0,
+      "title": ""
     }
   }]
 }
 
-const res = await getinfo();
-
-let widget = createWidget(res);
-Script.setWidget(widget);
-Script.complete();
-
-function createWidget(res) {
-    zhihuData = res;
-    const w = new ListWidget();
-
-  const fm = FileManager.iCloud();
-  const path = fm.documentsDirectory() + '/mm.jpg';
-  w.backgroundImage = fm.readImage(path);
-
-
-    w.addSpacer();
-    w.spacing = 5;
-
-    const firstLine = w.addText(`知乎热榜`);
-    firstLine.font = new Font('SF Mono', 15);
-    firstLine.textColor = Color.white();
-    firstLine.textOpacity = 0.7;
-
-    for (var i = 0; i < num; i++) {
-      addTextToListWidget(i + 1, zhihuData["data"][i], w);
-    }
-
-    w.addSpacer();
-    w.spacing = 5;
-    w.presentSmall();
-    return w;
-}
-
-async function getinfo() {
-  const url = {
-    url: `https://api.zhihu.com/topstory/hot-lists/total?limit=10&reverse_order=0`,
-  };
-  const res = await $.get(url);
-  log(res);
-  return res;
-}
-
 function addTextToListWidget(n, zhiData, listWidget) {
   let item = listWidget.addText(n + '. ' + zhiData["target"].title);
-
-
-    item.textColor = Color.white();
-  
-  item.font = new Font('SF Mono', 12);
+  item.textColor = Color.white();
+  item.font = new Font('SF Mono', 15);
   item.url = "zhihu://questions/" + zhiData["target"].id;
 }
 
-function color16() {
-  var r = Math.floor(Math.random() * 256);
-  if (r + 50 < 255) {
-    r = r + 50;
-  }
-  if (r > 230 && r < 255) {
-    r = r - 50;
-  }
-  var g = Math.floor(Math.random() * 256);
-  if (g + 50 < 255) {
-    g = g + 50;
-  }
-  if (g > 230 && g < 255) {
-    g = g - 50;
-  }
-  var b = Math.floor(Math.random() * 256);
-  if (b + 50 < 255) {
-    b = b + 50;
-  }
-  if (b > 230 && b < 255) {
-    b = b - 50;
-  }
-  var color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
-  return color;
+const data = await common.getDataFromCache("ZhihuCache", `https://api.zhihu.com/topstory/hot-lists/total?limit=10&reverse_order=0`, "GET", {}, {}, jsonCacheSeconds);
+const widget = await createWidget(data);
+if (!config.runsInWidget) {
+  await widget.presentMedium();
 }
+Script.setWidget(widget);
+Script.complete();
 
+async function createWidget(data) {
+  zhihuData = data;
+  let w = new ListWidget();
+  // 创建标题
+  var stackHeader = w.addStack()
+  stackHeader.centerAlignContent()
+  // 添加 icon
+  var stackImgItem = stackHeader.addImage(await common.getImgFromCache("Zhihu_app_logo.png", "https://static.zhihu.com/heifetz/favicon.ico", imgUseCache, cachePath))
+  stackImgItem.imageSize = new Size(15, 15)
+  stackImgItem.cornerRadius = 5
+  stackHeader.addSpacer(3)
+  // 添加标题
+  var textItem = stackHeader.addText("知乎热榜")
+  textItem.font = new Font('SF Mono', 15);
+  textItem.textColor = Color.white()
+
+  // 添加更新时间
+  stackHeader.addSpacer()
+  var myDate = new Date();
+  var textItem = stackHeader.addText(`${myDate.getHours().toString().padStart(2, '0')}:${myDate.getMinutes().toString().padStart(2, '0')}更新`)
+  textItem.font = Font.regularMonospacedSystemFont(10)
+  textItem.textColor = Color.white()
+  textItem.rightAlignText()
+
+  for (var i = 0; i < num; i++) {
+    addTextToListWidget(i + 1, zhihuData["data"][i], w);
+  }
+
+  w.spacing = 5;
+
+  // 设置透明壁纸 
+  const fm = FileManager.iCloud();
+  const path = fm.documentsDirectory() + '/mm.jpg';
+  await fm.downloadFileFromiCloud(path);
+  w.backgroundImage = fm.readImage(path);
+  return w;
+}
